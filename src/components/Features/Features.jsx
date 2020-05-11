e34645caedcda724e98a68880f23913048dc0d75
Rafael Urbina <rafaelurbinan@hotmail.com>
2020-05-11 10:02:21 -0400

    changing config.yml to add We Offer Section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,15 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useStaticQuery, graphql } from 'gatsby';
+
+
+const StyledContainer = styled.section`
+
+    width: 100%;
+
+
+`;
+
+const StyledTitle = styled.h4`
+
+    width: 100%;
+
+    text-align: center;
+
+
+`;
+
+
+const StyledList = styled.ul`
+
+    width: 80%;
+    margin: 0 auto;
+    list-style-type: none;
+`;
 
 
 
+const StyledListItem = styled.li`
 
+
+
+`;
+
+const FeaturesList = ({ featuresList }) => {
+
+    return featuresList.map((feature, index) => {
+        return(
+            <StyledListItem key={index}>{feature.feature_name}</StyledListItem>
+        );
+    });
+
+};
 
 function Features() {
+
+    const data = useStaticQuery(graphql`
+        query {
+            contentJson(id: { eq: "f3d66aa2-9c82-5a41-9c74-bec6ebcec3f1" }) {
+                title
+                features {
+                    feature_name
+                }
+            }
+        }
+    `);
+
+    const title = data.contentJson.title;
+    const features = data.contentJson.features;
+
+
     return (
-        <div>
-            
-        </div>
+        <StyledContainer>
+            <StyledTitle>{title}</StyledTitle>
+            <StyledList>
+                <FeaturesList featuresList={features} />
+            </StyledList>
+        </StyledContainer>
     );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import '../FontAwesome/library';
 import HomeGallery from "../components/HomeGallery/HomeGallery";
+import Features from "../components/Features/Features";
 
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />    
     <HomeGallery />   
+    <Features />
   </Layout>
 );
 
