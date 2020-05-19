41c37a82470ef8a5f3a60fcdefc167fd6e753c1c
Rafael Urbina <rafaelurbinan@hotmail.com>
2020-05-19 18:11:52 -0400

    fixing issue

---
+++ src/components/Redirecting/Redirecting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Redirecting() {
+    return (
+        <div>
+            REDIRECTING....
+        </div>
+    );
+}
+
+export default Redirecting;
--- src/pages/app.js
+++ src/pages/app.js
@@ -5,15 +5,18 @@ import SignIn from '../components/SignIn/SignIn';
 import Tutoriales from '../components/Tutoriales/Tutoriales';
 import NotFound from '../components/NotFound/NotFound';
 import PrivateRoute from '../components/PrivateRoute/PrivateRoute';
+import Redirecting from '../components/Redirecting/Redirecting';
 
 
 
 function app() {
     return (
-        <Router basepath="/app">            
+        <Router basepath="/app">  
+            <Redirecting path="/" />            
             <PrivateRoute path="/tutoriales" component={Tutoriales} />            
             <SignIn path="/signin" />
-            <Login path="/login" />             
+            <Login path="/login" />  
+            <NotFound path="/:route" />                  
         </Router>
     );
 }
