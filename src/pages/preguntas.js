049d7af99b02d511a7f5e8fc2ff704124d1a3be3
Rafael Urbina <rafaelurbinan@hotmail.com>
2020-10-25 14:55:29 -0400

    adding Questions Page

--- src/pages/preguntas.js
+++ src/pages/preguntas.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Layout from '../components/layout';
+import styled from 'styled-components';
+
+
+const StyledContainer = styled.section`
+
+    width: 100%;
+
+    
+
+
+`;
+
+
+
+
 
 function preguntas() {
     return (
-        <div>
-            HELLO FROM PREGUNTAS       
-        </div>
+        <Layout selectedIndex={2}>
+          HELLO FROM PREGUNTAS  
+        </Layout>
     );
 }
 
