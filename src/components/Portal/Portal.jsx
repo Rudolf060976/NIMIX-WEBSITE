cb71579d66d259509e6543703d1f790f97c62111
Rafael Urbina <rafaelurbinan@hotmail.com>
2020-10-27 21:30:02 -0400

    fixing Portal Component

--- src/components/Portal/Portal.jsx
+++ src/components/Portal/Portal.jsx
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 // Use a ternary operator to make sure that the document object is defined
 const portalRoot = typeof document !== `undefined` ? document.getElementById('portal') : null
 
+// Use a ternary operator to make sure that the document object is defined
+const el = typeof document !== `undefined` ? document.createElement('div') : null
+
 
 function Portal({ children }) {
-
-     // Use a ternary operator to make sure that the document object is defined
-     const el = typeof document !== `undefined` ? document.createElement('div') : null
 
 
     useEffect(() => {
@@ -22,7 +22,6 @@ function Portal({ children }) {
 
 
     if(el) {
-
         return ReactDOM.createPortal(children, el);
     } else {
         return null;
